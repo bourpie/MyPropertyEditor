fc41bd73a0ce00c2b4835c89ffce7df4dc908dcf
pbourgeois <pbourgeois@ORDPBOURGEOIS10>
2019-07-26 08:16:49 -0400

    Intégration du picker

--- MyPropertyEditor/App_Plugins/MyPropertyEditor/js/MyPropertyEditor.controller.js
+++ MyPropertyEditor/App_Plugins/MyPropertyEditor/js/MyPropertyEditor.controller.js
@@ -1,5 +1,31 @@
-﻿function myPropertyEditorController($scope, $http) {
+﻿function myPropertyEditorController($scope, $http, editorService) {
+    $scope.add = function () {
 
+        /* Définition d'une modale du infinit editor */
+        var giphyPicker = {
+            apikey: $scope.model.config.apikey,
+            title: "Animated Gif Search",
+            view: "/App_Plugins/MyPropertyEditor/MyPicker.html",
+            /* Appel à la petite fenêtre vertical de droite */
+            size: "small",
+            submit: function (model) {
+                $scope.model.value = model.images.original.url;
+                editorService.close();
+            },
+            close: function () {
+                editorService.close();
+            }
+        };
+
+        /**
+         * Appel du service Infinite Editor *
+         */
+        editorService.open(giphyPicker);
+    };
+
+    $scope.remove = function () {
+        $scope.model.value = null;
+    };
 }
 
 angular.module("umbraco").controller("myPropertyEditorController", myPropertyEditorController);
